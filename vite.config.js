5fda05e822a2c5e658c8d6ba660fde5f0130c09d
Geoffrey Hunt <info@runningdeveloper.com>
2025-08-14 22:22:43 +0200

    try github pages site

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/silly-cat-game/" : "/",
   publicDir: "public", // Explicitly set the public directory
   build: {
     target: "es2023", // Updated to ES2023 for 2025
